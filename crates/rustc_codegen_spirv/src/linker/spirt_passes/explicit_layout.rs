3ceaec4002d8806a1ec94f1019c02029606927c9
Eduard-Mihai Burtescu <eddyb@vectorware.com>
2025-09-09 11:40:02 +0300

    Update `spirt` for the new `mem`/`qptr` split.

--- crates/rustc_codegen_spirv/src/linker/spirt_passes/explicit_layout.rs
+++ crates/rustc_codegen_spirv/src/linker/spirt_passes/explicit_layout.rs
@@ -591,6 +591,11 @@ impl<'a> SelectiveEraser<'a> {
                 return Ok(());
             }
 
+            DataInstKind::Mem(_) => {
+                return Err(Diag::bug([
+                    "unhandled pointer type change in unexpected `mem` instruction".into(),
+                ]));
+            }
             DataInstKind::QPtr(_) => {
                 return Err(Diag::bug([
                     "unhandled pointer type change in unexpected `qptr` instruction".into(),
